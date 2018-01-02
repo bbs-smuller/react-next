38a076035cbb300064d388c385685a4a723bcd56
Sébastien Muller <smuller@big-boss-studio.com>
2018-01-02 16:26:16 +0100

    Try next/head component

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Header from './Header'
 
 const layoutStyle = {
@@ -8,6 +9,10 @@ const layoutStyle = {
 
 const Layout = (props) => (
 	<div style={layoutStyle}>
+		<Head>
+			<title>React NextJS</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width" key="meta_viewport"/>
+		</Head>
 		<Header/>
 		{props.children}
 	</div>
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
 import Layout from '../components/AppLayout'
 
 export default () => (
 	<Layout>
+		<Head>
+			<title>React - NextJS _ About page</title>
+			<meta name="viewport" content="initial-scale=1.2, width=device-width" key="meta_viewport"/>
+		</Head>
 		<p>This is the about page.</p>
 	</Layout>
 )
