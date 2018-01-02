218474f4fab6f950787b7785b3867732bd4a6381
Sébastien Muller <smuller@big-boss-studio.com>
2018-01-02 16:47:56 +0100

    Refactor about.js as a React Component

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,23 @@
 import Head from 'next/head'
+import React from 'react'
 import Layout from '../components/AppLayout'
 
-export default () => (
-	<Layout>
-		<Head>
-			<title>React - NextJS _ About page</title>
-			<meta name="viewport" content="initial-scale=1.2, width=device-width" key="meta_viewport"/>
-		</Head>
-		<p>This is the about page.</p>
-	</Layout>
-)
+export default class AboutPage extends React.Component {
+	static async getInitialProps({ req }) {
+		const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+
+		return { userAgent }
+	}
+
+	render() {
+		return (
+			<Layout>
+				<Head>
+					<title>React - NextJS _ About page</title>
+					<meta name="viewport" content="initial-scale=1.2, width=device-width" key="meta_viewport"/>
+				</Head>
+				<p>This is the about page on "{this.props.userAgent}".</p>
+			</Layout>
+		)
+	}
+}
